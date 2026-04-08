76af66543e375ecc3dc492d0da5c4aebb4da896b
Anand Prabhu <anandprabhu95@outlook.com>
2026-04-08 21:03:16 +0200

    Load wav file

--- testalg/testalg.py
+++ testalg/testalg.py
@@ -2,34 +2,45 @@ import numpy as np
 import random
 import math
 import matplotlib.pyplot as plt
+import scipy
 
-gSampleRate = 48000
+
+
 gBuffLen = 512
 gChannels = 2
 gBuff = np.zeros((gChannels, gBuffLen), np.float32)
-gAllpassBuff = np.zeros(3*gSampleRate)
 gSecsOfAudio = 10
 PI = 3.1416
 
-
 #=============================================================================================================        
-def sineArray(freq: float):
+def sineArray(freq: float, sampleRate):
     arr = []
     k = 0
     time = []
     for i in range (0, gSecsOfAudio * gSampleRate):
         arr.append(math.sin(2 * PI * freq * k))
-        k = k + 1/gSampleRate
+        k = k + 1/sampleRate
         time.append(k)
     #plt.plot(time, arr)
     #plt.show()
     return np.array(arr)
+
+
+def loadWav(file: str):
+    wav = scipy.io.wavfile.read(file)
+    print(str(type(wav[1][1])))
+    sampleRate = wav[0]
+    audioData = np.ndarray((gChannels, len(wav[1])),  np.float32)
+    for channel in range(0,gChannels):
+        for i in range(0,len(wav[1])):
+            audioData[channel,i] = wav[1][i,channel]/(2**31)
+    return sampleRate, audioData
     
     
 def stageBuffer(aud: np.ndarray, readPos: int, channel: int):
     global gBuff   
     for i in range(0,gBuffLen):
-        gBuff[channel,i] = aud[readPos+i]
+        gBuff[channel,i] = aud[channel,readPos+i]
         assert(len(gBuff[channel,:]) == gBuffLen)
     
 
@@ -42,23 +53,26 @@ def writeOutput(buff: np.ndarray, writePos: int, channel: int):
 def processBuffer(buff: np.ndarray):
     # youtAlg
     return 0
-
     
-#=============================================================================================================   
-aud = sineArray(10)
-samples = len(aud)
+    
+#=============================================================================================================
+wavData = loadWav(r"untitled.wav")
+aud = wavData[1]
+sampleRate = wavData[0]
 start = 0
-output = np.zeros((gChannels, len(aud)), np.float32)
+output = np.zeros((gChannels, len(aud[0,:])), np.float32)
+samples = len(aud[0,:])
+
 
 
 while(samples > 0):
-    if (start > len(aud)-gSampleRate):
+    if (start > len(aud[0,:])-gBuffLen):
         print("Finished")
         break
     
     for channel in range(0, gChannels):
         stageBuffer(aud, start, channel)
-    
+
     processBuffer(gBuff)
     
     for channel in range(0, gChannels):
@@ -67,11 +81,13 @@ while(samples > 0):
     start = start + gBuffLen
     samples = samples - gBuffLen
 
-    
-plt.plot(range(0,len(aud)),aud[:],'-',label='Input')
-plt.plot(range(0,len(aud)),output[0,:],'--',label='Output')
+
+plt.plot(range(0,len(aud[0,:])),aud[0,:],'-',label='Input')
+plt.plot(range(0,len(aud[0,:])),output[0,:],'--',label='Output')
 plt.legend(loc='upper right')
 plt.show()
+
+
     
 
     
