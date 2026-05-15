f09ba55d48054a85ef29d06cb22aa20ac927064e
Anand Prabhu <anandprabhu95@outlook.com>
2026-05-15 19:32:56 +0200

    ReverbStage class

--- testalg/testalg.py
+++ testalg/testalg.py
@@ -6,6 +6,96 @@ import scipy
 import time
 
 #=============================================================================================================   
+class Stream():
+    def __init__(self, audioData: np.ndarray, sampleRate: int, bufferSize: int, streamTime: float):
+        self.data = audioData
+        self.nChannels = len(audioData[:,0])
+        self.sampleRate = sampleRate
+        self.bufferSize = bufferSize 
+        self.readPosition = 0
+        self.writePosition = 0
+        self.streamTime = streamTime
+        self.time = 0
+        self.streamSamples = int(streamTime * sampleRate)
+        self.wavSamples = len(audioData[0,:])
+        self.endOfWavData = False
+        self.buffer = np.zeros((self.nChannels, bufferSize), np.float32)
+        self.out = np.zeros((self.nChannels, self.streamSamples+self.bufferSize), np.float32)
+    
+    def __elapsedTime__(self):
+        self.time = self.time + (self.bufferSize/self.sampleRate)
+               
+    def startStream(self): 
+        if (self.readPosition + self.bufferSize > self.streamSamples):
+            print("Finished")
+            return False
+
+        if (self.readPosition + self.bufferSize < self.wavSamples):
+            for channel in range(0, self.nChannels):
+                for i in range(0,self.bufferSize):
+                    self.buffer[channel,i] = self.data[channel,self.readPosition + i]
+
+        else:
+            if (self.endOfWavData == False):
+                end = self.wavSamples - self.readPosition
+                for channel in range(0, self.nChannels):
+                    for i in range(0,end):
+                        self.buffer[channel,i] = self.data[channel,self.readPosition + i]
+                    for i in range(end,self.bufferSize):
+                        self.buffer[channel,i] = 0
+                
+                self.endOfWavData = True
+            else:
+                for channel in range(0, self.nChannels):
+                    for i in range(0,self.bufferSize):
+                        self.buffer[channel,i] = 0
+                        
+        self.readPosition = self.readPosition + self.bufferSize
+        self.__elapsedTime__()
+        return True
+        
+    def output(self):
+        for channel in range(0, self.nChannels):
+            for i in range(0,self.bufferSize):
+                self.out[channel, self.writePosition+i] = self.buffer[channel,i]
+        self.writePosition = self.writePosition + self.bufferSize
+
+
+class WaveFile:        
+    @staticmethod    
+    def load(file: str):
+        sampleRate, wav = scipy.io.wavfile.read(file)
+        audioData = np.ndarray((2, len(wav)),  np.float32)
+        
+        if (wav.dtype == 'int16'):
+            normFactor = 15
+        elif(wav.dtype == 'int32'):
+            normFactor = 31
+        else:
+            raise ValueError("Bit-depth not supported. Load a 16 or 32 bit int wav file.")
+        
+        for channel in range(0,2):
+            for i in range(0,len(wav)):
+                audioData[channel,i] = wav[i,channel]/(2**normFactor)
+        
+        return sampleRate, audioData
+        
+    @staticmethod   
+    def write(filename: str, data: np.ndarray, sampleRate: int, bitDepth: str):
+        if (bitDepth == 'int16'):
+            dtype = np.int16
+        elif(bitDepth == 'int32'):
+            dtype = np.int32
+        else:
+            raise ValueError("Bit-depth not supported.")
+            
+        amplitude = np.iinfo(dtype).max
+        writeData = amplitude * data
+        print("Peaks: " + str(np.min(writeData)) + "," + str(np.max(writeData)))
+        writeData = np.clip(writeData, -amplitude, amplitude).T   
+        scipy.io.wavfile.write(filename, sampleRate, writeData.astype(dtype))   
+
+
 class Items:
         pass
 
@@ -88,61 +178,6 @@ class DelayLine:
                 for i in range(0,bufferSize-end):
                     buffer[channel,end+i] = self.delayBuffer[channel,i]*gain
                             
-
-class Stream():
-    def __init__(self, audioData: np.ndarray, sampleRate: int, bufferSize: int, streamTime: float):
-        self.data = audioData
-        self.nChannels = len(audioData[:,0])
-        self.sampleRate = sampleRate
-        self.bufferSize = bufferSize 
-        self.readPosition = 0
-        self.writePosition = 0
-        self.streamTime = streamTime
-        self.time = 0
-        self.streamSamples = int(streamTime * sampleRate)
-        self.wavSamples = len(audioData[0,:])
-        self.endOfWavData = False
-        self.buffer = np.zeros((self.nChannels, bufferSize), np.float32)
-        self.out = np.zeros((self.nChannels, self.streamSamples+self.bufferSize), np.float32)
-    
-    def __elapsedTime__(self):
-        self.time = self.time + (self.bufferSize/self.sampleRate)
-               
-    def startStream(self): 
-        if (self.readPosition + self.bufferSize > self.streamSamples):
-            print("Finished")
-            return False
-
-        if (self.readPosition + self.bufferSize < self.wavSamples):
-            for channel in range(0, self.nChannels):
-                for i in range(0,self.bufferSize):
-                    self.buffer[channel,i] = self.data[channel,self.readPosition + i]
-
-        else:
-            if (self.endOfWavData == False):
-                end = self.wavSamples - self.readPosition
-                for channel in range(0, self.nChannels):
-                    for i in range(0,end):
-                        self.buffer[channel,i] = self.data[channel,self.readPosition + i]
-                    for i in range(end,self.bufferSize):
-                        self.buffer[channel,i] = 0
-                
-                self.endOfWavData = True
-            else:
-                for channel in range(0, self.nChannels):
-                    for i in range(0,self.bufferSize):
-                        self.buffer[channel,i] = 0
-                        
-        self.readPosition = self.readPosition + self.bufferSize
-        self.__elapsedTime__()
-        return True
-        
-    def output(self):
-        for channel in range(0, self.nChannels):
-            for i in range(0,self.bufferSize):
-                self.out[channel, self.writePosition+i] = self.buffer[channel,i]
-        self.writePosition = self.writePosition + self.bufferSize
-
         
 class Diffuse8():
     def __init__(self, bufferSize: int):
@@ -183,113 +218,115 @@ class Diffuse8():
             for channel in range(0,self.nChannels):
                 exec(f"d.buffer{str(i)}[{channel},:] = result[{self.nChannels*i+channel},:]",locals()) 
                     
-class WaveFile:        
-    @staticmethod    
-    def load(file: str):
-        sampleRate, wav = scipy.io.wavfile.read(file)
-        audioData = np.ndarray((2, len(wav)),  np.float32)
-        
-        if (wav.dtype == 'int16'):
-            normFactor = 15
-        elif(wav.dtype == 'int32'):
-            normFactor = 31
-        else:
-            raise ValueError("Bit-depth not supported. Load a 16 or 32 bit int wav file.")
-        
-        for channel in range(0,2):
-            for i in range(0,len(wav)):
-                audioData[channel,i] = wav[i,channel]/(2**normFactor)
-        
-        return sampleRate, audioData
-        
-    @staticmethod   
-    def write(filename: str, data: np.ndarray, sampleRate: int, bitDepth: str):
-        if (bitDepth == 'int16'):
-            dtype = np.int16
-        elif(bitDepth == 'int32'):
-            dtype = np.int32
-        else:
-            raise ValueError("Bit-depth not supported.")
-            
-        amplitude = np.iinfo(dtype).max
-        writeData = amplitude * data
-        print("Peaks: " + str(np.min(writeData)) + "," + str(np.max(writeData)))
-        writeData = np.clip(writeData, -amplitude, amplitude).T   
-        scipy.io.wavfile.write(filename, sampleRate, writeData.astype(dtype))
-    
-    
-def processBuffer(buff: np.ndarray, proc: Items):
-    
-    for i in range(0,8):
-        exec(f"proc.bufferForDiffusion.buffer{str(i)}.fill(0)",locals())
-    
-    for i in range(0,8):
-        exec(f"proc.bufferForFdBkDelay.buffer{str(i)}.fill(0)",locals())
-   
-    print("INFO: Populating delay buffers")   
-    
-    splitGain = 0.0125
-    
-    proc.delayLine0.delay(buff)
-    proc.delayLine0.copyToBuffer(proc.bufferForDiffusion.buffer0,splitGain)
-    proc.delayLine1.delay(buff)
-    proc.delayLine1.copyToBuffer(proc.bufferForDiffusion.buffer1,splitGain)
-    proc.delayLine2.delay(buff)    
-    proc.delayLine2.copyToBuffer(proc.bufferForDiffusion.buffer2,splitGain)
-    proc.delayLine3.delay(buff)    
-    proc.delayLine3.copyToBuffer(proc.bufferForDiffusion.buffer3,splitGain)
-    proc.delayLine4.delay(buff)
-    proc.delayLine4.copyToBuffer(proc.bufferForDiffusion.buffer4,splitGain)
-    proc.delayLine5.delay(buff)
-    proc.delayLine5.copyToBuffer(proc.bufferForDiffusion.buffer5,splitGain)
-    proc.delayLine6.delay(buff)
-    proc.delayLine6.copyToBuffer(proc.bufferForDiffusion.buffer6,splitGain)
-    proc.delayLine7.delay(buff)
-    proc.delayLine7.copyToBuffer(proc.bufferForDiffusion.buffer7,splitGain)
 
-    print("INFO: Mixing buffers")   
-    proc.diffuser.diffuse(proc.bufferForDiffusion)
+class ReverbStage():
+    def __init__(self, bufferSize: int, sampleRate: int, nChannels: int, name: str):
+        self.bufferSize = bufferSize
+        self.sampleRate = sampleRate
+        self.nChannels = nChannels
+        self.name = name
+        
+        self.bufferForDiffusion = Items()
+        for i in range(0,8):
+            exec(f"self.bufferForDiffusion.buffer{str(i)} = np.zeros((self.nChannels, self.bufferSize))")
+            
+        self.bufferForFdBkDelay = Items()
+        for i in range(0,8):
+            exec(f"self.bufferForFdBkDelay.buffer{str(i)} = np.zeros((self.nChannels, self.bufferSize))")
+            
+        self.delayLine0 = DelayLine(0.06,self.sampleRate, self.nChannels)
+        self.delayLine1 = DelayLine(0.05,self.sampleRate, self.nChannels)
+        self.delayLine2 = DelayLine(0.09,self.sampleRate, self.nChannels)
+        self.delayLine3 = DelayLine(0.07,self.sampleRate, self.nChannels)
+        self.delayLine4 = DelayLine(0.08,self.sampleRate, self.nChannels)
+        self.delayLine5 = DelayLine(0.10,self.sampleRate, self.nChannels)
+        self.delayLine6 = DelayLine(0.12,self.sampleRate, self.nChannels)
+        self.delayLine7 = DelayLine(0.14,self.sampleRate, self.nChannels)
+        
+        
+        self.diffuser = Diffuse8(self.bufferSize)
+        
+        self.delayLineFdBk0 = DelayLine(0.02,self.sampleRate, self.nChannels)
+        self.delayLineFdBk1 = DelayLine(0.03,self.sampleRate, self.nChannels)
+        self.delayLineFdBk2 = DelayLine(0.04,self.sampleRate, self.nChannels)
+        self.delayLineFdBk3 = DelayLine(0.05,self.sampleRate, self.nChannels)
+        self.delayLineFdBk4 = DelayLine(0.06,self.sampleRate, self.nChannels)
+        self.delayLineFdBk5 = DelayLine(0.07,self.sampleRate, self.nChannels)
+        self.delayLineFdBk6 = DelayLine(0.09,self.sampleRate, self.nChannels)
+        self.delayLineFdBk7 = DelayLine(0.12,self.sampleRate, self.nChannels)
+        
+            
+    def process(self, buffer: np.ndarray):
+        for i in range(0,8):
+            exec(f"self.bufferForDiffusion.buffer{str(i)}.fill(0)",locals())
+        
+        for i in range(0,8):
+            exec(f"self.bufferForFdBkDelay.buffer{str(i)}.fill(0)",locals())
     
-    print("INFO: Feedback Delay")
-    np.add(proc.bufferForDiffusion.buffer0,proc.bufferForFdBkDelay.buffer0,out=proc.bufferForFdBkDelay.buffer0)
-    np.add(proc.bufferForDiffusion.buffer1,proc.bufferForFdBkDelay.buffer1,out=proc.bufferForFdBkDelay.buffer1)
-    np.add(proc.bufferForDiffusion.buffer2,proc.bufferForFdBkDelay.buffer2,out=proc.bufferForFdBkDelay.buffer2)
-    np.add(proc.bufferForDiffusion.buffer3,proc.bufferForFdBkDelay.buffer3,out=proc.bufferForFdBkDelay.buffer3)
-    np.add(proc.bufferForDiffusion.buffer4,proc.bufferForFdBkDelay.buffer4,out=proc.bufferForFdBkDelay.buffer4)
-    np.add(proc.bufferForDiffusion.buffer5,proc.bufferForFdBkDelay.buffer5,out=proc.bufferForFdBkDelay.buffer5)
-    np.add(proc.bufferForDiffusion.buffer6,proc.bufferForFdBkDelay.buffer6,out=proc.bufferForFdBkDelay.buffer6)
-    np.add(proc.bufferForDiffusion.buffer7,proc.bufferForFdBkDelay.buffer7,out=proc.bufferForFdBkDelay.buffer7)
+        print("INFO:",self.name,"Populating delay buffers")   
+        
+        splitGain = 0.0125
+        
+        self.delayLine0.delay(buffer)
+        self.delayLine0.copyToBuffer(self.bufferForDiffusion.buffer0,splitGain)
+        self.delayLine1.delay(buffer)
+        self.delayLine1.copyToBuffer(self.bufferForDiffusion.buffer1,splitGain)
+        self.delayLine2.delay(buffer)
+        self.delayLine2.copyToBuffer(self.bufferForDiffusion.buffer2,splitGain)
+        self.delayLine3.delay(buffer)
+        self.delayLine3.copyToBuffer(self.bufferForDiffusion.buffer3,splitGain)
+        self.delayLine4.delay(buffer)
+        self.delayLine4.copyToBuffer(self.bufferForDiffusion.buffer4,splitGain)
+        self.delayLine5.delay(buffer)
+        self.delayLine5.copyToBuffer(self.bufferForDiffusion.buffer5,splitGain)
+        self.delayLine6.delay(buffer)
+        self.delayLine6.copyToBuffer(self.bufferForDiffusion.buffer6,splitGain)
+        self.delayLine7.delay(buffer)
+        self.delayLine7.copyToBuffer(self.bufferForDiffusion.buffer7,splitGain)
     
-    gain = 0.8
-    
-    proc.delayLineFdBk0.addToBuffer(proc.bufferForFdBkDelay.buffer0,gain)
-    proc.delayLineFdBk0.delay(proc.bufferForFdBkDelay.buffer0)
-    
-    proc.delayLineFdBk1.addToBuffer(proc.bufferForFdBkDelay.buffer1,gain)
-    proc.delayLineFdBk1.delay(proc.bufferForFdBkDelay.buffer1)
+        print("INFO:",self.name,"Mixing buffers")   
+        self.diffuser.diffuse(self.bufferForDiffusion)
         
-    proc.delayLineFdBk2.addToBuffer(proc.bufferForFdBkDelay.buffer2,gain)
-    proc.delayLineFdBk2.delay(proc.bufferForFdBkDelay.buffer2)    
+        print("INFO:",self.name,"Feedback Delay")
+        np.add(self.bufferForDiffusion.buffer0,self.bufferForFdBkDelay.buffer0,out=self.bufferForFdBkDelay.buffer0)
+        np.add(self.bufferForDiffusion.buffer1,self.bufferForFdBkDelay.buffer1,out=self.bufferForFdBkDelay.buffer1)
+        np.add(self.bufferForDiffusion.buffer2,self.bufferForFdBkDelay.buffer2,out=self.bufferForFdBkDelay.buffer2)
+        np.add(self.bufferForDiffusion.buffer3,self.bufferForFdBkDelay.buffer3,out=self.bufferForFdBkDelay.buffer3)
+        np.add(self.bufferForDiffusion.buffer4,self.bufferForFdBkDelay.buffer4,out=self.bufferForFdBkDelay.buffer4)
+        np.add(self.bufferForDiffusion.buffer5,self.bufferForFdBkDelay.buffer5,out=self.bufferForFdBkDelay.buffer5)
+        np.add(self.bufferForDiffusion.buffer6,self.bufferForFdBkDelay.buffer6,out=self.bufferForFdBkDelay.buffer6)
+        np.add(self.bufferForDiffusion.buffer7,self.bufferForFdBkDelay.buffer7,out=self.bufferForFdBkDelay.buffer7)
         
-    proc.delayLineFdBk3.addToBuffer(proc.bufferForFdBkDelay.buffer3,gain)
-    proc.delayLineFdBk3.delay(proc.bufferForFdBkDelay.buffer3)    
+        gain = 0.8
         
-    proc.delayLineFdBk4.addToBuffer(proc.bufferForFdBkDelay.buffer4,gain)
-    proc.delayLineFdBk4.delay(proc.bufferForFdBkDelay.buffer4)
+        self.delayLineFdBk0.addToBuffer(self.bufferForFdBkDelay.buffer0,gain)
+        self.delayLineFdBk0.delay(self.bufferForFdBkDelay.buffer0)
         
-    proc.delayLineFdBk5.addToBuffer(proc.bufferForFdBkDelay.buffer5,gain)
-    proc.delayLineFdBk5.delay(proc.bufferForFdBkDelay.buffer5)
+        self.delayLineFdBk1.addToBuffer(self.bufferForFdBkDelay.buffer1,gain)
+        self.delayLineFdBk1.delay(self.bufferForFdBkDelay.buffer1)
         
-    proc.delayLineFdBk6.addToBuffer(proc.bufferForFdBkDelay.buffer6,gain)
-    proc.delayLineFdBk6.delay(proc.bufferForFdBkDelay.buffer6)
-    
-    proc.delayLineFdBk7.addToBuffer(proc.bufferForFdBkDelay.buffer7,gain)
-    proc.delayLineFdBk7.delay(proc.bufferForFdBkDelay.buffer7)
+        self.delayLineFdBk2.addToBuffer(self.bufferForFdBkDelay.buffer2,gain)
+        self.delayLineFdBk2.delay(self.bufferForFdBkDelay.buffer2)    
         
+        self.delayLineFdBk3.addToBuffer(self.bufferForFdBkDelay.buffer3,gain)
+        self.delayLineFdBk3.delay(self.bufferForFdBkDelay.buffer3)    
         
-    for k in range(0,8):
-        exec(f"np.add(buff,proc.bufferForFdBkDelay.buffer{k},out=buff)",locals(),globals())
-    return 0
+        self.delayLineFdBk4.addToBuffer(self.bufferForFdBkDelay.buffer4,gain)
+        self.delayLineFdBk4.delay(self.bufferForFdBkDelay.buffer4)
+        
+        self.delayLineFdBk5.addToBuffer(self.bufferForFdBkDelay.buffer5,gain)
+        self.delayLineFdBk5.delay(self.bufferForFdBkDelay.buffer5)
+        
+        self.delayLineFdBk6.addToBuffer(self.bufferForFdBkDelay.buffer6,gain)
+        self.delayLineFdBk6.delay(self.bufferForFdBkDelay.buffer6)
+        
+        self.delayLineFdBk7.addToBuffer(self.bufferForFdBkDelay.buffer7,gain)
+        self.delayLineFdBk7.delay(self.bufferForFdBkDelay.buffer7)
+            
+            
+        for k in range(0,8):
+            exec(f"np.add(buffer,self.bufferForFdBkDelay.buffer{k},out=buffer)",locals(),globals())
+        return 0
 
    
 #=============================================================================================================
@@ -297,53 +334,18 @@ sampleRate, aud = WaveFile.load(r"untitledpiano.wav")
 
 stream = Stream(audioData=aud, sampleRate=sampleRate, bufferSize=512, streamTime=5)
 
-c = Items()
-c.sampleRate = stream.sampleRate
-c.bufferSize = stream.bufferSize
-c.nChannels = stream.nChannels
-
-c.bufferForDiffusion = Items()
-
-for i in range(0,8):
-    exec(f"c.bufferForDiffusion.buffer{str(i)} = np.zeros((stream.nChannels, stream.bufferSize))")
-    
-c.bufferForFdBkDelay = Items()
-
-for i in range(0,8):
-    exec(f"c.bufferForFdBkDelay.buffer{str(i)} = np.zeros((stream.nChannels, stream.bufferSize))")
-    
-c.delayLine0 = DelayLine(0.06,stream.sampleRate, stream.nChannels)
-c.delayLine1 = DelayLine(0.05,stream.sampleRate, stream.nChannels)
-c.delayLine2 = DelayLine(0.09,stream.sampleRate, stream.nChannels)
-c.delayLine3 = DelayLine(0.07,stream.sampleRate, stream.nChannels)
-c.delayLine4 = DelayLine(0.08,stream.sampleRate, stream.nChannels)
-c.delayLine5 = DelayLine(0.10,stream.sampleRate, stream.nChannels)
-c.delayLine6 = DelayLine(0.12,stream.sampleRate, stream.nChannels)
-c.delayLine7 = DelayLine(0.14,stream.sampleRate, stream.nChannels)
-
-
-c.diffuser = Diffuse8(stream.bufferSize)
-
-c.delayLineFdBk0 = DelayLine(0.02,stream.sampleRate, stream.nChannels)
-c.delayLineFdBk1 = DelayLine(0.03,stream.sampleRate, stream.nChannels)
-c.delayLineFdBk2 = DelayLine(0.04,stream.sampleRate, stream.nChannels)
-c.delayLineFdBk3 = DelayLine(0.05,stream.sampleRate, stream.nChannels)
-c.delayLineFdBk4 = DelayLine(0.06,stream.sampleRate, stream.nChannels)
-c.delayLineFdBk5 = DelayLine(0.07,stream.sampleRate, stream.nChannels)
-c.delayLineFdBk6 = DelayLine(0.09,stream.sampleRate, stream.nChannels)
-c.delayLineFdBk7 = DelayLine(0.12,stream.sampleRate, stream.nChannels)
-
+reverb = ReverbStage(bufferSize=stream.bufferSize, sampleRate=stream.sampleRate, nChannels=stream.nChannels, name='Stage 1') 
 
 flag = True
 
 while(flag):    
     flag = stream.startStream()
-    print("Time: " + str(round(stream.time,4)) + "s")
+    print("Time:",str(round(stream.time,4)),"s")
     starttime = time.process_time()
-    processBuffer(stream.buffer, c)
+    reverb.process(stream.buffer)
     endtime = time.process_time()
     stream.output()   
-    print("ExecTime: " + str(endtime-starttime))
+    print("ExecTime:",str(endtime-starttime))
 
 WaveFile.write(r"output.wav", stream.out, sampleRate, 'int32')
 
